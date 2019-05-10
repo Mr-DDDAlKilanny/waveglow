aee2a12b2f68010ed15ae024e5241a59ce0f0d1c
Ibraheem AlKilanny <ibrahimalkilanny@gmail.com>
2019-05-10 07:32:49 +0300

    segement fix

--- mel2samp.py
+++ mel2samp.py
@@ -65,6 +65,7 @@ class Mel2Samp(torch.utils.data.Dataset):
     def __init__(self, training_files, segment_length, filter_length,
                  hop_length, win_length, sampling_rate, mel_fmin, mel_fmax):
         self.audio_files = files_to_list(training_files)
+        self.audio_files_segment_pos = [0 for _ in self.audio_files]
         random.seed(1234)
         random.shuffle(self.audio_files)
         self.stft = TacotronSTFT(filter_length=filter_length,
@@ -94,7 +95,10 @@ class Mel2Samp(torch.utils.data.Dataset):
         # Take segment
         if audio.size(0) >= self.segment_length:
             max_audio_start = audio.size(0) - self.segment_length
-            audio_start = random.randint(0, max_audio_start)
+            audio_start = self.audio_files_segment_pos[index]
+            self.audio_files_segment_pos[index] += (self.sampling_rate // random.randint(1, 20))
+            if self.audio_files_segment_pos[index] > max_audio_start:
+                self.audio_files_segment_pos[index] = random.randint(0, max_audio_start)
             audio = audio[audio_start:audio_start+self.segment_length]
         else:
             audio = torch.nn.functional.pad(audio, (0, self.segment_length - audio.size(0)), 'constant').data
